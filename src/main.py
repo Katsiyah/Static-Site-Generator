4b328423a9e3921f96eb0e7669159900660221a8
hades <aurorabellalis0@gmail.com>
2026-05-09 14:37:43 +1000

    created TextNode and TextType

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+
+def main():
+    new = TextNode("hello world", TextType("bold"), "http")
+    print(new)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,26 @@
+from enum import Enum 
+
+class TextType(Enum):
+    PLAIN = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINKS = "link"
+    IMAGES = "img"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, other):
+        if self.text == other.text:
+            if self.text_type == other.text_type:
+                if self.url == other.url:
+                    return True
+        return False
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
